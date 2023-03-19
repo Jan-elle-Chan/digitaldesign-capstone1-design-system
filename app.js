4aa3df2ce19f5c7dc52f11a22fedf14d79f4e91e
Jan-elle-Chan <j_michelene@yahoo.com>
2023-03-18 18:21:52 -0700

    Delete unnecessary js

--- app.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    var copyText = document.getElementById("myInput");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
-    
-    var tooltip = document.getElementById("myTooltip");
-    tooltip.innerHTML = "Copied: " + copyText.value;
-  }
